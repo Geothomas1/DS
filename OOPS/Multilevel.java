5a73f68332dbfb852bdb3252fc2840bb7c099e19
Geo Thomas <geothomas@cet.ac.in>
2021-05-13 21:53:49 +0530

    Multilevel inheritance

---
+++ OOPS/Multilevel.java
@@ -0,0 +1,21 @@
+
+class Emp {
+    String name = "Geo Thomas";
+    int age = 23;
+
+}
+
+class Salary extends Emp {
+    int Salary = 2000;
+}
+
+class Multilevel extends Salary {
+    public static void main(String[] args) {
+        Multilevel m = new Multilevel();
+        System.out.println(m.name);
+        System.out.println(m.age);
+        System.out.println(m.Salary);
+
+    }
+
+}
