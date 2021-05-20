f2c9813c3a5aab71892efe53f2b4ef82a16d9847
Geo Thomas <geothomas@cet.ac.in>
2021-05-20 20:33:33 +0530

    Java Exception

---
+++ Java Exceptions/Exception1.java
@@ -0,0 +1,15 @@
+public class Exception1 {
+    
+    public static void main(String[] args) {
+        try{
+        int[] arr={10,20,30};
+        System.out.println(arr[5]);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception catched from catch block");
+
+        }
+        
+    }
+}
