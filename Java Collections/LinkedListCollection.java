64c4967122b6c14d2fd7f681792449f906a4738c
Geo Thomas <geothomas@cet.ac.in>
2021-05-20 20:08:04 +0530

    LinkedList collection

---
+++ Java Collections/LinkedListCollection.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+public class LinkedListCollection {
+    public static void main(String[] args) {
+        LinkedList<String> list=new LinkedList<String>();
+        list.add("BMW");
+        list.add("Benze");
+        list.add("Maruthi");
+        System.out.println(list);
+
+        
+    }
+    
+}
