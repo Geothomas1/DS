dba85016707f0b80883bf8eef2e99a08f6d202f0
Geo Thomas <geothomas@cet.ac.in>
2021-06-17 22:43:15 +0530

    Selection sort !

---
+++ Sorting/selectionSort.java
@@ -0,0 +1,28 @@
+
+public class selectionSort {
+    public static void main(String args[]){
+        
+        int temp;
+        int[] arr={43,54,3,67,34,23,98,70,10};
+        int minIndex=0;
+        for(int i=0;i<arr.length;i++){
+            minIndex=i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+                temp=arr[minIndex];
+                arr[minIndex]=arr[i];
+                arr[i]=temp;
+                     
+
+            }
+                    
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
